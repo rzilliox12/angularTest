018afaf6321fa1ad671bed3cb1324d9ea0539482
Ray Zilliox <rzilliox12@yahoo.com>
2020-07-18 11:39:23 -0400

    toh-pt3

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -9,13 +9,7 @@ import { HEROES } from '../mock-heros'
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
-
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
